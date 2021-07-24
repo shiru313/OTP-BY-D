49872ae32da71dd8b1a52c3825a1f37b4209c4a4
SHIRU n00b <72118394+shiru313@users.noreply.github.com>
2021-07-24 17:52:43 +0530

    Add files via upload

---
+++ boot.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+clear
+GREEN='\e[32m'
+RED='\e[31m'
+echo
+echo
+echo    #coder by noob hacker 
+echo
+echo
+echo
+echo -e "${RED} use own risk I do not take responsibility for anything that happens with this 100 times"
+echo "[✔]===================================≠=========≠========================================[✔]";
+echo
+sleep 2.0
+clear
+echo "[✔]===================================≠=========≠================================"
+echo                #SH1RF4AN MLP N008 H4CK36
+sleep 1.0
+clear                                        
+echo -e "${RED} use own risk I do not take responsibility for anything that happens with this 100 times"
+echo "[✔]===================================≠=========≠========================================[✔]";
+echo                                                                       
+echo                                                                      
+echo
+echo "created by noob hacker "
+echo
+echo      
+echo                                                                       
+echo "press 'y' continue"
+echo
+read -p "Are you sure? " -n 1 -r
+echo    
+if [[ ! $REPLY =~ ^[Yy]$ ]]   #wa.me/+919895967245
+then
+    [[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1 
+fi
+
+
+clear
+apt update
+apt upgrade
+clear
+bash worker.sh
---
+++ code.sh
@@ -0,0 +1,4 @@
+RED="\e[31m"
+ENDCOLOR="\e[0m"
+
+echo -e "${RED}Red text${ENDCOLOR}"
